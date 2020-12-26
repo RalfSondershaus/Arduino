04041e5ae84086660d06e337d62d9ba5b68fd5af
Ralf Sondershaus <ralf.sondershaus@gmx.de>
2020-12-26 12:41:41 +0100

    Enhanced BlinkSampple, removed SequenceBlink

--- Gen/Timer/Timer.h
+++ Gen/Timer/Timer.h
@@ -1,14 +1,18 @@
+/**
+ * Declares and defines classes 
+ * - MicroTimer based on microseconds 
+ * - MilliTimer based on milliseconds
+ */
+
 #ifndef TIMER_H__
 #define TIMER_H__
 
 #include <Arduino.h>
-/**
-* Declares and defines class MicroTimer
-*/
 
+// --------------------------------------------------------------------------------------------
 /// Start a timer, get time since a timer start and check a timeout.
 /// Is based on micros(), so all units are [us].
-/// 70 minutes.
+/// Maximal time: 70 minutes.
 /// <br>
 /// Example 1:
 /// <code>
@@ -28,7 +32,7 @@
 /// t.timeout(); // returns true if timeout() is called at least 1000 us after t.start(1000)
 /// ...
 /// </code>
-
+// --------------------------------------------------------------------------------------------
 class MicroTimer
 {
 public:
@@ -36,8 +40,8 @@ public:
   typedef unsigned long tTimer;
 
 protected:
-  /// time stamp at start()
-    tTimer ulTimer;
+  /// time stamp for timeout
+  tTimer ulTimer;
 
 public:
   /// Construct
@@ -49,21 +53,20 @@ public:
   /// Start timer, return timer value in [us]
   tTimer start(tTimer ulTime = 0)   { ulTimer = micros() + ulTime; return ulTimer; }
 
-  /// Get time since start() in [us]
+  /// Get time since start() + ulTime in [us]
   tTimer getTimeSince(void) const          { return micros() - ulTimer; }
 
   /// Return true if timer is elapsed; false otherwise
   /// micros() >= ulTimer: positive result: highest bit is 0
   /// micros() <  ulTimer: negative result: highest bit is 1
-  bool timeout(void)                           { return (((tTimer) (micros() - ulTimer)) & (tTimer) 0x80000000u) == 0u; }
+  bool timeout(void) const                 { return (((tTimer) (micros() - ulTimer)) & (tTimer) 0x80000000u) == 0u; }
 };
 
-/**
-* Declares and defines class MilliTimer
-*/
+// --------------------------------------------------------------------------------------------
 /// Start a timer, get time since a timer start and check a timeout.
 /// Is based on millis(), so all units are [ms].
-/// 50 days.
+/// Maximal time: 50 days.
+// --------------------------------------------------------------------------------------------
 class MilliTimer
 {
 public:
@@ -83,13 +86,13 @@ public:
   /// Start timer, return timer value in [ms]
   tTimer start(tTimer ulTime = 0)           { ulTimer = millis() + ulTime; return ulTimer; }
 
-  /// Get time since start() in [ms]
+  /// Get time since start() + ulTime in [ms]
   tTimer getTimeSince(void) const           { return millis() - ulTimer; }
 
   /// Return true if timer is elapsed; false otherwise
   /// millis() >= ulTimer: positive result: highest bit is 0
   /// millis() <  ulTimer: negative result: highest bit is 1
-  boolean timeout(void)                           { return (((tTimer) (millis() - ulTimer)) & (tTimer) 0x80000000u) == 0u; }
+  boolean timeout(void) const               { return (((tTimer) (millis() - ulTimer)) & (tTimer) 0x80000000u) == 0u; }
 };
 
 #endif // TIMER_H__
