96c6bd087ff14e3277ecc375caea6776f0a6855a
RalfSondershaus <ralf.sondershaus@gmx.de>
2025-05-25 17:00:54 +0200

    Try to fix compilation error on Linux with ssize_t

--- Src/Gen/Test/unity/src/unity_adapt.h
+++ Src/Gen/Test/unity/src/unity_adapt.h
@@ -33,8 +33,8 @@
 /// @tparam T Type to be checked such as uint8, int, size_t
 /// @param actual current value
 /// @param expected expected value
-template<typename T>
-void EXPECT_EQ(T actual, T expected)
+template<typename T1, typename T2>
+void EXPECT_EQ(T1 actual, T2 expected)
 {
   // not implemented yet
   // std::cout << typeid(T).name() << std::endl;
@@ -46,7 +46,7 @@ void EXPECT_EQ(T actual, T expected)
 /// @param actual current value
 /// @param expected expected value
 template<>
-void EXPECT_EQ<bool>(bool actual, bool expected)
+void EXPECT_EQ<bool, bool>(bool actual, bool expected)
 {
   if (expected)
   {
@@ -60,15 +60,16 @@ void EXPECT_EQ<bool>(bool actual, bool expected)
 
 // Remark: [C++11 standard, 3.9.1 Fundamental types] "Plain char, signed char, and unsigned char are three distinct types."
 
-template<> void EXPECT_EQ<char>(char actual, char expected)        { TEST_ASSERT_EQUAL_CHAR(expected, actual); }
-template<> void EXPECT_EQ<sint8>(sint8 actual, sint8 expected)     { TEST_ASSERT_EQUAL_INT8(expected, actual); }
-template<> void EXPECT_EQ<sint16>(sint16 actual, sint16 expected)  { TEST_ASSERT_EQUAL_INT16(expected, actual); }
-template<> void EXPECT_EQ<sint32>(sint32 actual, sint32 expected)  { TEST_ASSERT_EQUAL_INT32(expected, actual); }
-template<> void EXPECT_EQ<sint64>(sint64 actual, sint64 expected)  { TEST_ASSERT_EQUAL_INT64(expected, actual); }
-template<> void EXPECT_EQ<uint8>(uint8 actual, uint8 expected)     { TEST_ASSERT_EQUAL_UINT8(expected, actual); }
-template<> void EXPECT_EQ<uint16>(uint16 actual, uint16 expected)  { TEST_ASSERT_EQUAL_UINT16(expected, actual); }
-template<> void EXPECT_EQ<uint32>(uint32 actual, uint32 expected)  { TEST_ASSERT_EQUAL_UINT32(expected, actual); }
-template<> void EXPECT_EQ<uint64>(uint64 actual, uint64 expected)  { TEST_ASSERT_EQUAL_UINT64(expected, actual); }
+template<> void EXPECT_EQ<char, char>(char actual, char expected)        { TEST_ASSERT_EQUAL_CHAR(expected, actual); }
+template<> void EXPECT_EQ<sint8,sint8>(sint8 actual, sint8 expected)     { TEST_ASSERT_EQUAL_INT8(expected, actual); }
+template<> void EXPECT_EQ<sint16,sint16>(sint16 actual, sint16 expected)  { TEST_ASSERT_EQUAL_INT16(expected, actual); }
+template<> void EXPECT_EQ<sint32,sint32>(sint32 actual, sint32 expected)  { TEST_ASSERT_EQUAL_INT32(expected, actual); }
+template<> void EXPECT_EQ<sint64,sint64>(sint64 actual, sint64 expected)  { TEST_ASSERT_EQUAL_INT64(expected, actual); }
+template<> void EXPECT_EQ<uint8,uint8>(uint8 actual, uint8 expected)     { TEST_ASSERT_EQUAL_UINT8(expected, actual); }
+template<> void EXPECT_EQ<uint16,uint16>(uint16 actual, uint16 expected)  { TEST_ASSERT_EQUAL_UINT16(expected, actual); }
+template<> void EXPECT_EQ<uint32,uint32>(uint32 actual, uint32 expected)  { TEST_ASSERT_EQUAL_UINT32(expected, actual); }
+template<> void EXPECT_EQ<uint64,uint64>(uint64 actual, uint64 expected)  { TEST_ASSERT_EQUAL_UINT64(expected, actual); }
+template<> void EXPECT_EQ<uint64,uint32>(uint64 actual, uint32 expected)  { TEST_ASSERT_EQUAL_UINT64(static_cast<uint64>(expected), actual); }
 template<> void EXPECT_EQ<util::streampos>(util::streampos actual, util::streampos expected) { EXPECT_EQ<util::streamoff>(expected, actual); }
 
 /// Declares a test function
