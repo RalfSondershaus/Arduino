845f52459739f828df779643eb61ef296f2ffd30
Ralf Sondershaus <ralf.sondershaus@gmx.de>
2025-06-01 09:16:45 +0200

    Added docu

--- Src/Prj/App/Signal/Com/AsciiCom.h
+++ Src/Prj/App/Signal/Com/AsciiCom.h
@@ -1,9 +1,36 @@
 /**
  * @file Signal/Com/AsciiCom.h
  *
- * @brief
+ * @brief Handles ASCII-based telegram communication for signal control.
  *
- * @copyright Copyright 2024 Ralf Sondershaus
+ * The AsciiCom class is responsible for receiving,
+ * processing, and responding to ASCII telegrams related to signal configuration
+ * and status queries. The supported telegrams allow setting and getting signal
+ * aspects, blinks, targets, input classification, and change-over times, as well
+ * as copying signal configurations between IDs.
+ *
+ * Example telegrams:
+ * - SET_SIGNAL 0 ASPECTS 11000 00100 00000 00000 00000
+ * - SET_SIGNAL 0 BLINKS  00110 11001 11111 00000 00000
+ * - SET_SIGNAL 0 TARGETS ONBOARD pin ONBOARD pin ONB pin
+ * - SET_SIGNAL 0 INPUT CLASSIFIED 1
+ * - SET_SIGNAL 0 COT 10 20
+ * - GET_SIGNAL 0 ASPECTS
+ * - GET_SIGNAL 0 TARGETS
+ * - SET_CLASSIFIER 1 PIN 5
+ * - SET_CLASSIFIER 1 LIMITS 2 10 20
+ * - SET_CLASSIFIER 1 DEBOUNCE 5
+ * - GET_CLASSIFIER 1 PIN
+ * - GET_CLASSIFIER 1 LIMITS
+ * - GET_CLASSIFIER 1 DEBOUNCE
+ *
+ * @note The signal ID is a number from 0 to 63. Aspects and blinks are bit values.
+ * Target ports can be ONBOARD, ONB, EXTERN, or EXT, followed by a pin number (0-31).
+ * Change-over times are two values in [10 ms].
+ * 
+ * @note Parts of the documentation of this file was created by GitHub Copilot.
+ * 
+ * @copyright Copyright 2024-2025 Ralf Sondershaus
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -30,29 +57,40 @@ namespace com
 {
 
   // -----------------------------------------------------------------------------------
-  /// <payload>'\n'
+  /// Receives telegrams and processes them:
   ///
-  /// With <payload> such as:
-  ///
+  /// SET_SIGNAL group:
   /// \code
-  /// SET_SIGNAL id ASPECTS [aspect]+                          set (5) bits for aspect and blink (e.g. 11000)
-  /// SET_SIGNAL id BLINKS  [blink]+                           set (5) bits for aspect and blink (e.g. 00000)
+  /// SET_SIGNAL id ASPECTS [aspect]+                          Set the aspects for a signal. 
+  ///                                                          [aspects]+ can include up to five aspects. 
+  ///                                                          If fewer than five aspects are provided, only the specified aspects are updated; the remaining aspects will stay unchanged
+  /// SET_SIGNAL id BLINKS  [blink]+                           set (5) bits for blink (e.g. 00000)
   /// SET_SIGNAL id TARGETS [(ONBOARD,ONB,EXTERN, EXT) pin]+   set (5) target ports for signal id
   /// SET_SIGNAL id INPUT CLASSIFIED id                        set input: type is classifier with ID id
   /// SET_SIGNAL id COT t1 t2                                  set change over times
-  /// 
-  /// SET_SIGNAL_ASPECTS    id 11000 00100 00110 11001 11111
-  /// SET_SIGNAL_BLINKS     id 00000 00000 00000 00000 00000
-  /// SET_SIGNAL_TARGETS    id ONB pin ONBOARD pin ONBOARD pin ONBOARD pin ONB pin
-  /// SET_SIGNAL_INPUT      id ONBOARD class_id
-  /// SET_SIGNAL_COT        id t1 t2
+  /// \endcode
   ///
+  /// id is the signal ID, which is a number from 0 to 63.
+  /// aspect is a bit value, blink is a bit value, pin is a pin number (0-31).
+  /// The target ports can be ONBOARD, ONB, EXTERN, or EXT, followed by a pin number.
+  /// The change over times are two values in [10 ms].
+  ///  
+  /// Examples:
+  /// \code
+  /// SET_SIGNAL 0 ASPECTS 11000 00100 00110 11001 11111      sets aspects for signal 0
+  /// SET_SIGNAL 0 BLINKS  00000 00000 00000 00000 00000      sets blinks for signal 0
+  /// SET_SIGNAL 0 TARGETS ONBOARD pin ONBOARD pin ONB pin    sets target ports for signal 0
+  /// SET_SIGNAL 0 INPUT CLASSIFIED 1                         sets input for signal 0 to classifier with ID 1
+  /// SET_SIGNAL 0 COT 10 20                                  sets change over times for signal 0 to 100 ms and 200 ms
+  /// \endcode
+  ///
+  /// \code
   /// GET_SIGNAL id ASPECTS                         returns all (5) aspects for signal id
   ///
   /// GET_SIGNAL 0 ASPECT                           
   /// GET_SIGNAL 0 TARGET                           returns all (5) target ports for signal 0
   ///
-  /// CPY_SIGNAL 0 SIGNAL 1                         copies all aspects and target ports from signal 0 to signal 1
+  /// SET_CLASSIFIER id [PIN, LIMITS, DEBOUNCE] ...
   /// \code
   // -----------------------------------------------------------------------------------
   class AsciiCom : public Observer
--- Src/Prj/App/Signal/Doc/Readme.md
+++ Src/Prj/App/Signal/Doc/Readme.md
@@ -30,7 +30,47 @@ Each signal is controlled by a DCC command. This is experimental.
 
 ## Configuration
 
-You can configure the application with a serial monitor.
+The application can be configured via a serial interface using the AsciiCom protocol. With a serial terminal, you can send messages to the application and receive messages from it.
+
+Note: The maximal length of a message is 64 characters.
+
+### Supported Commands
+
+#### SET_SIGNAL
+
+| Command | Description                | Example Usage         |
+|----------------|----------------------------|----------------------|
+| `SET_SIGNAL id ASPECTS [aspect]+` | Set the aspects for a signal. `[aspect]+` can include up to `kNrSignalAspects` aspects. If fewer than `kNrSignalAspects` aspects are provided, only the specified aspects are updated; the remaining aspects will stay unchanged. A `1` means that the corresponding LED is on, `0` means off. The order of the bits corresponds to the order of the pins, see `SET_SIGNAL id TARGETS`. |`SET_SIGNAL 0 ASPECTS 11000 00100 00110 11001 11111`<br>sets aspects for signal 0. With respect to the example of `SET_SIGNAL id TARGETS` below, `11000`, the left-mode `1` corresponds to pin 13, the right-most `0` to pin 9.|
+| `SET_SIGNAL id BLINKS [blink]+` | Set blink for a signal. `[blink]+` can include up to `kNrSignalAspects` blinks, so a blink per aspect. If fewer than `kNrSignalAspects` blinks are provided, only the specified blinks are updated; the remaining blinks will stay unchanged. A `1` means that the corresponding LED starts to blink, `0` means LED on without blink. | `SET_SIGNAL 0 BLINKS 00000 00000 00000 00000 00000`<br>signal 0 doesn't blink | 
+| `SET_SIGNAL id TARGETS [(ONBOARD, ONB, EXTERN, EXT) pin]+` | Sets up to `NrSignalTargets` target ports for a signal. EXTERN (EXT) are currently not supported. `pin` is the Arduino pin number. | `SET_SIGNAL 0 TARGETS ONB 13 ONB 12 ONB 11 ONB 10 ONB 9` Set 5 targets for signal 0: pins 13, 12, 11, 10, and 9. |
+| `SET_SIGNAL id INPUT CLASSIFIED id` | Sets input type: type is classifier with ID `id`. | `SET_SIGNAL 0 INPUT CLASSIFIED 0`<br>Assigns classifier 0 to signal 0. |
+| `SET_SIGNAL id COT t1 t2` |  Sets change-over times (`t1` and `t2` use [10 ms]). | `SET_SIGNAL 0 COT 10 20`<br> set change over time of signal 0 to 100 ms and blink change over time to 200 ms. |
+
+#### GET_SIGNAL
+
+| Command | Description                | Example Usage         |
+|----------------|----------------------------|----------------------|
+| `GET_SIGNAL id ASPECTS` | Returns all `kNrSignalAspects` aspects for a signal. | `GET_SIGNAL 0 ASPECTS` | 
+| `GET_SIGNAL id BLINKS` | Returns all `kNrSignalAspects` blinks for a signal. | `GET_SIGNAL 0 BLINKS` | 
+| `GET_SIGNAL id TARGETS` | Returns all `kNrSignalTargets` target ports for signal id. | `GET_SIGNAL 0 TARGETS` | 
+| `GET_SIGNAL id INPUT` | Returns the classifier and classifier ID. | `GET_SIGNAL 0 INPUT` | 
+| `GET_SIGNAL id COT` | Returns change over time [10 ms] and blink change over time [10 ms]. | `GET_SIGNAL 0 COT` | 
+
+#### SET_CLASSIFIER
+
+| Command | Description                | Example Usage         |
+|----------------|----------------------------|----------------------|
+| `SET_CLASSIFIER id PIN pin` | Sets the Arduino analog input pin (ADC). | `SET_CLASSIFIER 0 PIN 56`<br> Set analog input pin 56 for classifier 0. | 
+| `SET_CLASSIFIER id LIMITS slot-id min max` | Sets the AD limits for the given slot of the classifier. A classifier supports `kNrClassifierClasses` classes. `min` and `max` are given in raw values. The AD values are `4*min` and `4*max`. | `SET_CLASSIFIER 0 LIMITS 1 85 102`<br>sets limits to `85 (raw) = 4*85 = 340 (AD)` and `102 (raw) = 102*4 = 408` | 
+| `SET_CLASSIFIER id DEBOUNCE t1` | Sets debounce time [10 ms] for a classifier. | `SET_CLASSIFIER 0 DEBOUNCE 5`<br>set a debounce time of 50 ms for classifier 0. | 
+
+#### GET_CLASSIFIER
+
+| Command | Description                | Example Usage         |
+|----------------|----------------------------|----------------------|
+| `GET_CLASSIFIER id PIN` | Gets the Arduino analog input pin (ADC). | `GET_CLASSIFIER 0 PIN` | 
+| `GET_CLASSIFIER id LIMITS` | Gets the AD limits for the given slot of the classifier. A classifier supports `kNrClassifierClasses` classes. `min` and `max` are given in raw values. The AD values are `4*min` and `4*max`. | `GET_CLASSIFIER 0 LIMITS` | 
+| `GET_CLASSIFIER id DEBOUNCE` | Gets debounce time [10 ms] for a classifier. | `GET_CLASSIFIER 0 DEBOUNCE` | 
 
 ## Folder Structure
 
